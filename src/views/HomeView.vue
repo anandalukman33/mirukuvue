ad88fcc69ad42b97a59bf416ee5e4301a4c428a6
Ananda Muhamad Lukman <ananda.muhamad.lukman@bni.co.id>
2024-10-15 14:17:21 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <HeaderMiruku/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderMiruku from '@/components/HeaderMiruku.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderMiruku
+  }
+}
+</script>
